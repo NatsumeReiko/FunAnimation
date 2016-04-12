2376028550dc2b643ed98ce48e472f9c23f291b5
NatsumeReiko <sprix.reiko.natsume@gmail.com>
2016-04-12 15:28:20 +0900

    Initial commit

---
+++ app/src/main/java/amy/com/funanimation/RepeatStopActivity.java
@@ -0,0 +1,13 @@
+package amy.com.funanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepeatStopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repeat_stop);
+    }
+}
